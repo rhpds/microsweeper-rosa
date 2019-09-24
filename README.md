d82f23a8f3d4bcda2ae8ad0e4f1fc8dee5101e3f
jamesfalkner <schtool@gmail.com>
2019-09-24 14:09:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple Microsweeper game
+
+[![Developer Workspace](https://che.openshift.io/factory/resources/factory-contribute.svg)](http://codeready-codeready.apps.crwd.rh-apps.org/f?id=factoryrlwzl3x9v55hqlbu)
+
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require('path');
+
+var app = express();
+
+var staticPath = path.join(__dirname, '/static');
+app.use(express.static(staticPath));
+
+app.listen(3000, function() {
+  console.log('listening on port 3000');
+});
